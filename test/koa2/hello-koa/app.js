3f2d842a5e02e9b76fecb6152d997a53c31ec264
sundalei <sundalei2011@163.com>
2016-09-06 22:40:48 +0800

    201609061040

--- test/koa2/hello-koa/app.js
+++ test/koa2/hello-koa/app.js
@@ -5,7 +5,7 @@ const app = new Koa();
 app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>'
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
 
 app.listen(3000);
