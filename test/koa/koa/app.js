fed9c8e9665e444ea18dee256d0f09b6df40a3df
sundalei <sundalei2011@163.com>
2016-09-06 20:24:23 +0800

    201609062024

--- test/koa/koa/app.js
+++ test/koa/koa/app.js
@@ -1,20 +1,49 @@
 'use strict';
 
-var koa = require('koa');
+var 
+    koa = require('koa'),
+    fs = require('fs');
+
 var app = koa();
 
 app.use(function *() {
-    yield doReadFile1;
-    var data = yield doReadFile2;
+    yield doReadFile1();
+    var data = yield doReadFile2();
     this.body = data;
 });
 
 function doReadFile1() {
-    return 'hello';
+    return new Promise(function(resolve, reject) {
+        fs.readFile(__dirname + '/static/file1', 'utf8', function(err, data) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    }).then(function(data) {
+        console.log(data);
+        return data;
+    }).catch(function(reason) {
+        console.log(reason);
+    });
 }
 
 function doReadFile2() {
-    return 'world';
+    return new Promise(function(resolve, reject) {
+        fs.readFile(__dirname + '/static/file2', 'utf8', function(err, data) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    }).then(function(data) {
+        console.log(data);
+        return data;
+    }).catch(function(reason) {
+        console.log(reason);
+    });
 }
 
 app.listen(3000, function() {
