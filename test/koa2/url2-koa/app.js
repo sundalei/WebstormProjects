09dca5be100fc1eaa9ace1ff695f3cc4433a5337
sundalei <sundalei2011@163.com>
2016-09-08 21:01:52 +0800

    201609082101

--- test/koa2/url2-koa/app.js
+++ test/koa2/url2-koa/app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
+const fs = require('fs');
+
 const router = require('koa-router')();
 
 const app = new Koa();
@@ -13,33 +15,30 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-router.get('/hello/:name', async (ctx, next) => {    
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
+var files = fs.readdirSync(__dirname + '/controllers');
 
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-      <form action="/signin" method="post">
-        <p>Name: <input name="name" value="koa"></p>
-        <p>Password: <input type="password" name="password"></p>
-        <p><input type="submit" value="Submit"></p>
-      </form>`;
-});
+var js_files = files.filter((f) => {
+    return f.endsWith('.js');
+}, files);
 
-router.post('/signin', async (ctx, next) => {
-    var
-       name = ctx.request.body.name || '',
-       password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`
+for (var f of js_files) {
+    console.log(`process controller: ${f}...`);
+
+    let mapping = require(__dirname + '/controllers/' + f);
+    for (var url in mapping) {
+        if (url.startsWith('GET ')) {
+            var path = url.substring(4);
+            router.get(path, mapping[url]);
+            console.log(`register URL mapping: GET ${path}`);
+        } else if (url.startsWith('POST ')) {
+            var path = url.substring(5);
+            router.post(path, mapping[url]);
+            console.log(`register URL mapping: POST ${path}`);
+        } else {
+            console.log(`invalid URL: ${url}`);
+        }
     }
-    
-});
+}
 
 app.use(router.routes());
 
