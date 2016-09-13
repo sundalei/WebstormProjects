fd499a459a39684987bbb46345231a56bc01bedb
sundalei <sundalei2011@163.com>
2016-09-13 21:12:38 +0800

    201609132112

--- test/mysql/model-sequelize/db.js
+++ test/mysql/model-sequelize/db.js
@@ -62,10 +62,29 @@ function defineModel(name, attributes) {
     console.log('model defined for table: ' + name + '\n' + JSON.stringify(attrs, function (k, v) {
         if (k === 'type') {
             for (let key in Sequelize) {
-                console.log(key);
+                if (key === 'ABSTRACT' || key === 'NUMBER') {
+                    continue;
+                }
+                let dbType = Sequelize[key];
+
+                if (typeof dbType === 'function') {
+                    
+                    // STRING
+                    if (v instanceof dbType) {
+                        if (v._length) {
+                            return `${dbType.key}(${v._length})`;
+                        }
+                        return dbType.key;
+                    }
+
+                    // BIGINT, BOOLEAN
+                    if (v === dbType) {
+                        return dbType.key;
+                    }
+                }
             }
         }
-        console.log('-------------------------------');
+        
         return v;
     }, '  '));
     
@@ -75,6 +94,20 @@ function defineModel(name, attributes) {
         hooks: {
             beforeValidate: function(obj) {
                 console.log(obj);
+                let now = Date.now();
+                if (obj.isNewRecord) {
+                    console.log('will create entity...' + obj);
+                    if (!obj.id) {
+                        obj.id = generateId();
+                    }
+                    obj.createdAt = now;
+                    obj.updatedAt = now;
+                    obj.version = 0;
+                } else {
+                    console.log('will update entity...');
+                    obj.updatedAt = Date.now();
+                    obj.version++;
+                }
             }
         }
     });
@@ -83,7 +116,14 @@ function defineModel(name, attributes) {
 const TYPES = ['STRING', 'INTEGER', 'BIGINT', 'TEXT', 'DOUBLE', 'DATEONLY', 'BOOLEAN'];
 
 var exp = {
-    defineModel: defineModel
+    defineModel: defineModel,
+    sync: () => {
+        if (process.env.NODE_ENV !== 'production') {
+            sequelize.sync({ force: true });
+        } else {
+            throw new Error('Cannot sync() when NODE_ENV is set to \'production\'.');
+        }
+    }
 }
 
 for (let type of TYPES) {
