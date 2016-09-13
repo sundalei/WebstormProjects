3b22bb24f2c5303c04ebe8cbb2357ae56bef2ce5
sundalei <sundalei2011@163.com>
2016-09-13 22:58:54 +0800

    201609132258

---
+++ test/mysql/model-sequelize/config-override.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var config = {
+    database: 'production',
+    username: 'www',
+    password: 'secret-password',
+    host: '192.168.1.199'
+};
+
+module.exports = config;
---
+++ test/mysql/model-sequelize/config-test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var config = {
+    dialect: 'mysql',
+    database: 'test',
+    username: 'sundalei',
+    password: 'root',
+    host: 'localhost',
+    port: 3306
+};
+
+module.exports = config;
