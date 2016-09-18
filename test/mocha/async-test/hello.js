f8ceb88530b2558af2464ef1009c0e46c1013986
sundalei <sundalei2011@163.com>
2016-09-18 16:10:30 +0800

    201604101610

---
+++ test/mocha/async-test/hello.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('init hello.js...');
+
+const fs = require('mz/fs');
+
+module.exports = async () => {
+    let expression = await fs.readFile('./data.txt', 'utf8');
+    let fn = new Function('return ' + expression);
+    let r = fn();
+    console.log(`Calculate: ${expression} = ${r}`);
+    return r;
+};
---
+++ test/mocha/hello-test/hello.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('init hello.js...');
+
+module.exports = function (...rest) {
+    var sum = 0;
+    for (let n of rest) {
+        sum += n;
+    }
+    return sum;
+};
