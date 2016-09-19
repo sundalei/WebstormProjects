bcff4fbb7a55954a22f1e6047294ec7e2e2e0532
sundalei <sundalei2011@163.com>
2016-09-19 17:21:40 +0800

    201609191721

---
+++ test/websocket/hello-ws/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const WebSocket = require('ws');
+
+const WebSocketServer = WebSocket.Server;
+
+const wss = new WebSocketServer({
+    port: 3000
+});
+
+wss.on('connection', function (ws) {
+    console.log(`[SERVER] connection()`);
+    ws.on('message', function (message) {
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send(`ECHO: ${message}`, (err) => {
+            if (err) {
+                console.log(`[SERVER] error: ${err}`);
+            }
+        });
+    });
+});
