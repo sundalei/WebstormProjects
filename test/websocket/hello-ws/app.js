0a7d1932d9b1397a898a96b826099f6d28f5ec38
sundalei <sundalei2011@163.com>
2016-09-20 00:17:30 +0800

    201609200017

--- test/websocket/hello-ws/app.js
+++ test/websocket/hello-ws/app.js
@@ -12,10 +12,19 @@ wss.on('connection', function (ws) {
     console.log(`[SERVER] connection()`);
     ws.on('message', function (message) {
         console.log(`[SERVER] Received: ${message}`);
-        ws.send(`ECHO: ${message}`, (err) => {
-            if (err) {
-                console.log(`[SERVER] error: ${err}`);
-            }
-        });
+        if ('Goodbye' === message) {
+            ws.close();
+            return;
+        }
+        setTimeout(() => {
+            ws.send(`What's your name?`, (err) => {
+                if (err) {
+                    console.log(`[SERVER] error: ${err}`);
+                }
+            });
+        }, 1000);  
+    });
+    ws.on('close', function() {
+        console.log('[SERVER] disconnected');
     });
 });
