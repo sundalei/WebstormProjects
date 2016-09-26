93826b59da6554da5db9ddc7f87f29a2979a4e23
sundalei <sundalei2011@163.com>
2016-09-27 00:22:38 +0800

    201609270022

--- test/rest/rest-koa/app.js
+++ test/rest/rest-koa/app.js
@@ -4,6 +4,8 @@ const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
+const templating = require('./templating');
+
 const app = new Koa();
 
 // log request URL:
@@ -18,6 +20,12 @@ app.use(staticFile('/static/', __dirname + '/static'));
 
 // parse request body:
 app.use(bodyParser());
+
+// add nunjucks as view:
+app.use(templating('view', {
+    noCache: true,
+    watch: true
+}));
 
 app.listen(3000);
 console.log('app started at port 3000...');
