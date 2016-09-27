030a67e080c3a638afa26131a633b400f5723582
user1 <user1@user.local>
2016-09-27 14:13:11 +0800

    201609271413

--- test/rest/rest-koa/app.js
+++ test/rest/rest-koa/app.js
@@ -4,7 +4,11 @@ const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
+const controller = require('./controller');
+
 const templating = require('./templating');
+
+const rest = require('./rest');
 
 const app = new Koa();
 
@@ -26,6 +30,10 @@ app.use(templating('view', {
     noCache: true,
     watch: true
 }));
+
+app.use(rest.restify());
+
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
